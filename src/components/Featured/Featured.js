c031650dc9ffac43780a1c63c0f7ed671cc0f3d8
Samuel Trahan <samtrahan91@gmail.com>
2021-01-22 10:17:25 -0600

    some more stlyling and landing page coming together

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './Featured.css';
 import Caymus from '../../assets/caymus.jpg';
-import Sake from '../../assets/delight.jpg';
 import {SAKE} from '../../sakeDB';
 
 export default function Featured() {
+    
   
     return (
         <div className="featured">
-            <div className="xyz-in" xyz="fade up big">
+            <div className="wine-section-landing">
             <div className="wine-feature">
-                <img src={Caymus} alt=""></img>
-            </div>
+                <img className="image" src={Caymus} alt=""></img>
             </div>
             <div className="wine-description" >
                 <h1>Caymus</h1>
@@ -20,15 +19,24 @@ export default function Featured() {
                 <h2>Taste Notes</h2>
                 <h2>Pairing</h2>
             </div>
+            </div>
+            <div className="sake-section-landing">
             <div className="sake-feature">
-                <img className="sake-pic" src={Sake} alt=""></img>
+            <img className="image" alt="" src={SAKE[0].picture}></img>
             </div>
             <div className="sake-description">
-            <h1>Heaven of Tipsy Delight</h1>
+            {/* <h1>Heaven of Tipsy Delight</h1>
                 <h3>Vinyard:  Caymus Vineyards</h3>
                 <h2>Region</h2>
                 <h2>Taste Notes</h2>
-                <h2>Pairing</h2>
+                <h2>Pairing</h2> */}
+                <h1>{SAKE[0].name}</h1>
+                <h1>{SAKE[0].bottlePrice}</h1>
+                <h3>{SAKE[0].bottleSize}</h3>
+                <h3>Alcohol Percentage: {SAKE[0].ABV}</h3>
+                <h3>Food Pairing: {SAKE[0].foodPairing}</h3>
+                <h3>Tasting Notes: {SAKE[0].tastingNotes}</h3>
+            </div>
             </div>
             <div className="xyz-in" xyz="fade up big">I will animate in!</div>
         </div>
