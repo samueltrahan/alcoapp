3200e75433619a67bc29a308e725fc104f64116c
Samuel Trahan <samtrahan91@gmail.com>
2021-02-03 11:06:19 -0600

    wine dropdown in nav bar added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -24,6 +24,16 @@ export default function NavBar() {
             </div>
             <div className="nav-links">
                 <a className="drinks" href="/wines">Wines</a>
+                <div class="dropdown-comp ui compact menu">
+                <div class="dropdown-menu ui simple dropdown item">
+                     Wines
+                 <i class="dropdown icon"></i>
+                <div class="menu-list menu">
+                <a className="wine-links" href="/redwines">Red Wine</a><br />
+                <a className="wine-links" href="/whitewines">White Wine</a>
+                </div>
+                </div>
+                </div>
                 <a className="drinks" href="/sakes">Sakes</a>
                 <a className="drinks" href="/specialty">Specialty Drinks</a>
             </div>
